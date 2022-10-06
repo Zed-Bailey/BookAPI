23c5f0d29c899e048fa93268cff1cfda9871fa54
Zed laptop <emailzed03@gmail.com>
2022-10-06 14:22:47 +1100

    changed count from string to int

--- src/main/java/com/zed/bookapi/model/BookResponse.java
+++ src/main/java/com/zed/bookapi/model/BookResponse.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookResponse {
-    public String count;
+    public int count;
     public List<Books> results;
     public String error;
 
     public BookResponse(List<Books> booksList) {
-        this.count = String.valueOf(booksList.size());
+        this.count = booksList.size();
         this.results = booksList;
         this.error = "";
     }
 
     public BookResponse(String error) {
         this.error = error;
-        this.count = "0";
+        this.count = 0;
         this.results = new ArrayList<>();
     }
 
     public BookResponse(Books book) {
-        this.count = "1";
+        this.count = 1;
         this.error = "";
         this.results = List.of(book);
     }
