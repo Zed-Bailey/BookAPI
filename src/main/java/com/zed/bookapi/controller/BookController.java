34e51bd7a67800cd8e7a07d972f1b4858f247ec4
Zed laptop <emailzed03@gmail.com>
2022-10-06 14:23:11 +1100

    implemented all endpoints for the controller!

--- src/main/java/com/zed/bookapi/controller/BookController.java
+++ src/main/java/com/zed/bookapi/controller/BookController.java
@@ -1,16 +1,17 @@
 package com.zed.bookapi.controller;
 
 import com.zed.bookapi.model.BookResponse;
-import com.zed.bookapi.model.Books;
+import com.zed.bookapi.model.CategoryResponse;
 import com.zed.bookapi.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
+import java.time.LocalDate;
 
 @RestController
 public class BookController {
@@ -19,9 +20,9 @@ public class BookController {
     private BookService service;
 
 
-    @GetMapping("/")
-    public Books HelloWorld() {
-        return service.getBooks().get(0);
+    @GetMapping("/books")
+    public ResponseEntity<BookResponse> HelloWorld() {
+        return new ResponseEntity<>(new BookResponse(service.getBooks()), HttpStatus.OK);
     }
 
     @GetMapping("/books/{id}")
@@ -49,4 +50,59 @@ public class BookController {
         }
         return new ResponseEntity<>(new BookResponse(book), HttpStatus.OK);
     }
+
+    @GetMapping("/books/author")
+    public ResponseEntity<BookResponse> GetBooksByAuthor(@RequestParam(name = "v") String author) {
+        var authorBooks = service.getBooksByAuthor(author);
+        return new ResponseEntity<>(new BookResponse(authorBooks), HttpStatus.OK);
+    }
+
+    @GetMapping("/books/rating")
+    public ResponseEntity<BookResponse> getBooksWithRatingGreaterThan(@RequestParam(name = "gt") double gt) {
+        return new ResponseEntity<>(new BookResponse(service.getBooksWithRatingGreaterThan(gt)), HttpStatus.OK);
+    }
+
+    @GetMapping("/books/rating")
+    public ResponseEntity<BookResponse> getBooksWithRatingLessThan(@RequestParam(name = "lt") double lt) {
+        return new ResponseEntity<>(new BookResponse(service.getBooksWithRatingLessThan(lt)), HttpStatus.OK);
+    }
+
+    @GetMapping("/books/publishers")
+    public ResponseEntity<CategoryResponse> getAllPublishers() {
+        return new ResponseEntity<>(new CategoryResponse(service.getPublishers()), HttpStatus.OK);
+    }
+
+    @GetMapping("/books/published")
+    public ResponseEntity<BookResponse> getBooksPublished(@RequestParam(name = "by") String by) {
+        return new ResponseEntity<>(new BookResponse(service.getBooksByPublisher(by)), HttpStatus.OK);
+    }
+
+    @GetMapping("/books/published")
+    public BookResponse getBooksPublishedBefore(@RequestParam(name = "before") String publishedBefore) {
+        LocalDate date = LocalDate.parse(publishedBefore);
+        return new BookResponse(service.getBooksPublishedBefore(date));
+    }
+
+    @GetMapping("/books/published")
+    public BookResponse getBooksPublishedOn(@RequestParam(name = "on") String publishedOn) {
+        LocalDate date = LocalDate.parse(publishedOn);
+        return new BookResponse(service.getBooksPublishedOn(date));
+    }
+
+    @GetMapping("/books/published")
+    public BookResponse getBooksPublishedAfter(@RequestParam(name = "after") String publishedAfter) {
+        LocalDate date = LocalDate.parse(publishedAfter);
+        return new BookResponse(service.getBooksPublishedAfter(date));
+    }
+
+    @GetMapping("/books/languagecodes")
+    public ResponseEntity<CategoryResponse> getLanguageCodes() {
+        return new ResponseEntity<>(new CategoryResponse(service.getLanguageCodes()), HttpStatus.OK);
+    }
+
+    @GetMapping("/books")
+    public ResponseEntity<BookResponse> getBooksByLanguage(@RequestParam(name = "language") String code) {
+        return new ResponseEntity<>(new BookResponse(service.getBooksWithLanguageCode(code)), HttpStatus.OK);
+    }
+
 }
