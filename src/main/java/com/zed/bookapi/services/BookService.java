eebb97a0de9688cfac357844f84ee240a9e455ae
Zed laptop <emailzed03@gmail.com>
2022-10-06 11:10:04 +1100

    wired service functions to repository

--- src/main/java/com/zed/bookapi/services/BookService.java
+++ src/main/java/com/zed/bookapi/services/BookService.java
@@ -1,11 +1,12 @@
 package com.zed.bookapi.services;
 
-import com.zed.bookapi.BookRepository;
+import com.zed.bookapi.repository.BookRepository;
 import com.zed.bookapi.model.Books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -13,8 +14,26 @@ public class BookService {
     private BookRepository repository;
 
     public List<Books> getBooks() {
-//        var i = repository.findAll();
-//        return i;
         return (List<Books>) repository.findAll();
+    }
+
+
+    public Books getBookByISBN(String isbn) {
+        return repository.getBookByISBN(isbn);
+    }
+
+    public Books getBookByISBN13(String isbn) {
+        return repository.getBookByISBN13(isbn);
+    }
+
+
+    /**
+     * Returns the book with the corresponding ID or null if not found
+     *
+     * @param id the booksID
+     * @return returns Books model if ID is found else null
+     */
+    public Optional<Books> getBookByID(int id) {
+        return repository.findById(id);
     }
 }
