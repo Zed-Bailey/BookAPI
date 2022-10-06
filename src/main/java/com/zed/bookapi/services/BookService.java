374620892595485cdcb17c0a48ea718894d5d271
Zed laptop <emailzed03@gmail.com>
2022-10-06 14:08:50 +1100

    implemented all service methods

--- src/main/java/com/zed/bookapi/services/BookService.java
+++ src/main/java/com/zed/bookapi/services/BookService.java
@@ -5,6 +5,7 @@ import com.zed.bookapi.model.Books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +18,19 @@ public class BookService {
         return (List<Books>) repository.findAll();
     }
 
+    /**
+     * Returns the book with the corresponding ID
+     *
+     * @param id the booksID
+     * @return Optional book, value will be null if book not found
+     */
+    public Optional<Books> getBookByID(int id) {
+        return repository.findById(id);
+    }
+
+    public List<Books> getBooksByAuthor(String author) {
+        return (List<Books>) repository.getBooksByAuthor(author);
+    }
 
     public Books getBookByISBN(String isbn) {
         return repository.getBookByISBN(isbn);
@@ -26,14 +40,41 @@ public class BookService {
         return repository.getBookByISBN13(isbn);
     }
 
-
-    /**
-     * Returns the book with the corresponding ID or null if not found
-     *
-     * @param id the booksID
-     * @return returns Books model if ID is found else null
-     */
-    public Optional<Books> getBookByID(int id) {
-        return repository.findById(id);
+    public List<Books> getBooksWithRatingGreaterThan(double rating) {
+        return (List<Books>) repository.getBooksWithRatingGreaterThan(rating);
     }
+
+    public List<Books> getBooksWithRatingLessThan(double rating) {
+        return (List<Books>) repository.getBooksWithRatingLessThan(rating);
+    }
+
+    public List<String> getLanguageCodes() {
+        return (List<String>) repository.getLanguageCodes();
+    }
+
+    public List<Books> getBooksWithLanguageCode(String code) {
+        return (List<Books>) repository.getBooksByLanguageCode(code);
+    }
+
+    public List<String> getPublishers() {
+        return (List<String>) repository.getPublishers();
+    }
+
+    public List<Books> getBooksByPublisher(String publisher) {
+        return (List<Books>) repository.getBooksByPublisher(publisher);
+    }
+
+
+    public List<Books> getBooksPublishedBefore(LocalDate date) {
+        return (List<Books>) repository.getBooksPublishedBefore(date);
+    }
+
+    public List<Books> getBooksPublishedOn(LocalDate date) {
+        return (List<Books>) repository.getBooksPublishedOn(date);
+    }
+
+    public List<Books> getBooksPublishedAfter(LocalDate date) {
+        return (List<Books>) repository.getBooksPublishedAfter(date);
+    }
+
 }
