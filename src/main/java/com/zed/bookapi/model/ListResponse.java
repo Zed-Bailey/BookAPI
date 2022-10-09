a1c69b2d9c39e23847b957061ebd166b5c4001fb
Zed laptop <emailzed03@gmail.com>
2022-10-09 23:09:39 +1100

    renamed to a more appropriate name

--- src/main/java/com/zed/bookapi/model/ListResponse.java
+++ src/main/java/com/zed/bookapi/model/ListResponse.java
@@ -2,11 +2,14 @@ package com.zed.bookapi.model;
 
 import java.util.List;
 
-public class CategoryResponse {
+/**
+ * List response for the language codes and publishers endpoints
+ */
+public class ListResponse {
     public int count;
     public List<String> results;
 
-    public CategoryResponse(List<String> results) {
+    public ListResponse(List<String> results) {
         this.count = results.size();
         this.results = results;
     }
