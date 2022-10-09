06e61eb4cd2ebe10a0419ea49b92e5f0b829b760
Zed laptop <emailzed03@gmail.com>
2022-10-09 23:11:24 +1100

    playing around with testing the controller

--- src/test/java/com/zed/bookapi/BookapiApplicationTests.java
+++ src/test/java/com/zed/bookapi/BookapiApplicationTests.java
@@ -1,13 +1,38 @@
 package com.zed.bookapi;
 
+import com.zed.bookapi.controller.BookController;
+import com.zed.bookapi.services.BookService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
 class BookapiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private BookService service;
+
 	@Test
-	void contextLoads() {
+	void whenValidID_returns200() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders
+						.get("/books/1"))
+//				.andDo(print())
+				.andExpect(status().isOk());
 	}
 
 }
